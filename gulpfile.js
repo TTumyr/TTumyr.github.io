ba75dddfba262dcb69c0af2ae3b33c06f8e6dcaf
TTumyr <40184866+TTumyr@users.noreply.github.com>
2019-04-09 17:52:50 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+console.log('Working');
+});
+
+gulp.task('html', function() {
+    console.log('new line');
+});
+
